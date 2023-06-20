02f50c88ec232a190e590b863e02cc88a74f35b2
YFkeith <yfkeithlee@gmail.com>
2023-06-20 15:54:42 +0800

    test pwa

--- vite.config.js
+++ vite.config.js
@@ -7,13 +7,23 @@ export default defineConfig({
   plugins: [
     vue(),
     VitePWA({
-      includeAssets: ["favicon.ico"],
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
       manifest: {
         name: "My Awesome App",
         short_name: "MyApp",
         description: "My Awesome App description",
         background_color: "#000000",
         icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
           {
             src: "favicon.ico",
             sizes: "32x32",
