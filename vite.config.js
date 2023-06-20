3e341bed7a4651eb6fdb65fb45268fbef4dd1003
YFkeith <yfkeithlee@gmail.com>
2023-06-20 17:01:09 +0800

    test pwa

--- vite.config.js
+++ vite.config.js
@@ -12,8 +12,9 @@ export default defineConfig({
         name: "FunsGirl App",
         short_name: "FunsGirl",
         start_url: ".",
-        display: "fullscreen",
+        display: "standalone",
         description: "拯救宇宙",
+        theme_color: "#000000",
         background_color: "#000000",
         icons: [
           {
